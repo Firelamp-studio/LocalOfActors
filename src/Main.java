f02eaef23ee372e185d9835ed5129e467bf9ba05
Sim1onE <smitarcade@gmail.com>
2018-04-25 12:52:20 +0200

    dsdwwd

--- src/Main.java
+++ src/Main.java
@@ -9,53 +9,11 @@ import Game.Maps.BarMap;
 public class Main {
 	
     public static void main(String[] args) {
+    	//Non modificare!!! Lavorare dirattemante nella BarMap
     	
-    	/*Tail lista = new Tail();
-    	lista.addToTail(new Customer());
-    	lista.addToTail(new Customer());
-    	lista.addToTail(new Customer());
-    	System.out.println(lista.toString());
-    	lista.removeFirst();
-    	System.out.println(lista.toString());
-    	lista.removeFirst();
-    	System.out.println(lista.toString());
-    	lista.removeFirst();
-    	System.out.println(lista.toString());
-    	lista.removeFirst();
-    	System.out.println(lista.toString());*/
-
-    	/*
-        Barrel barrel = new Barrel();
-
-        Barman barman1 = new Barman();
-        Barman barman2 = new Barman();
-        Barman barman3 = new Barman();
-        Barman barman4 = new Barman();
-
-        barrel.bindActorForEvents(barman1);
-        barrel.bindActorForEvents(barman2);
-        barrel.bindActorForEvents(barman3);
-        barrel.bindActorForEvents(barman4);
-
-        barman1.consumaDaBarile(barrel, 50);
-        barman2.consumaDaBarile(barrel, 150);
-        barman3.consumaDaBarile(barrel, 250);
-        barman4.consumaDaBarile(barrel, 450);
-        barman1.consumaDaBarile(barrel, 100);
-        */
-
-        /*
-    	EntryDoor entryDoor = new EntryDoor();
-        entryDoor.faiEntrareQualcunoOgniTanto(5000);
-        */
-
-        /*
-        Rotator rotator = new Rotator(450);
-        System.out.println(rotator.getRotation());
-        */
-
         Map bar = new BarMap(new Vector(1500, 900));
         Window viewFrame = new Window(bar);
+        
     }
 }
 
