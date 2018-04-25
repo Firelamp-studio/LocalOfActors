73bd2a0c4e435ec8a46c147fb02fb72434e1a037
Lorenzo Pecchio <pecchio.lorenzo@gmail.com>
2018-04-25 23:22:21 +0200

    dsd

--- src/Game/Actors/Customer.java
+++ src/Game/Actors/Customer.java
@@ -1,11 +1,16 @@
 package Game.Actors;
 
+import java.util.Random;
+
 import API.Annotations.ActionCallable;
 import API.Annotations.ActionResponse;
+import API.Utility.TimerAction;
 import API.Utility.Vector;
 import Game.DrinkCard;
 
 public class Customer extends Person {
+	private TimerAction timerChooseAction;
+	private LocalTail localTail;
 	private DrinkCard drinkCard;
 	private CashDesk cashDesk;
 	
@@ -15,23 +20,40 @@ public class Customer extends Person {
 		} else {
 			setSprite("woman.png", 0.75);
 		}
+		
+		localTail = new LocalTail(30, 10, 10000, 60000);
+	}
+	
+	public Customer(LocalTail localTail) {
+		if(Math.random() > 0.5) {
+			setSprite("man.png", 0.75);
+		} else {
+			setSprite("woman.png", 0.75);
+		}
+		
+		this.localTail = localTail;
 	}
 	
 	@Override
 	protected void beginPlay() {
 		super.beginPlay();
 		
-		moveTo(new Vector(500));
+		actionCallResponse(localTail, "get-in-line-for-entry", this);
 	}
 
     @Override
     protected void tick(long deltaTime) {
-
+    	
     }
-	
+    
+	@ActionResponse(name = "get-in-line-for-entry")
+	public void getInLineForEntry(Vector vector) {
+		moveTo(vector);
+	}
+
 	@ActionCallable(name = "entry-into-local")
 	public void moveToCashdesk() {
-		moveTo(cashDesk, "arrived-to-cashdesk");
+		moveTo(this, "arrived-to-cashdesk");
 	}
 	
 	@ActionCallable(name = "arrived-to-cashdesk")
@@ -40,12 +62,18 @@ public class Customer extends Person {
 	}
 	
 	@ActionResponse(name = "pay-and-get-card")
-	public void onCardRecived() {
-		//TODO Fai il cazzo che vuoi dentro il locale
+	public void onCardRecived(DrinkCard drinkCard) {
+		this.drinkCard = drinkCard;
+		/*long delay = new Random().nextInt(maxWaitTimeMS) + 1;
+		timerChooseAction = new TimerAction(true, delay, this, "choose-what-to-do");
+		timerChooseAction.execute();*/
+		//timerChooseAction.
 	}
 	
-    public void setDrinkCard(DrinkCard drinkCard) {
-    	this.drinkCard = drinkCard;
-    }
+	@ActionCallable(name = "choose-what-to-do")
+	public void chooseWahtToDo() {
+		
+	}
+	
     
 }
