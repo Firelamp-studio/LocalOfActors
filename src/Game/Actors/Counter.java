a4fee3307e900f969a19a142fd36e3efa19f7ce6
Lorenzo Pecchio <pecchio.lorenzo@gmail.com>
2018-05-06 17:48:07 +0200

    dfgfgdfgdfg

--- src/Game/Actors/Counter.java
+++ src/Game/Actors/Counter.java
@@ -12,21 +12,22 @@ public class Counter extends Actor {
 	public Counter(CounterTail counterTail) {
 		this.counterTail = counterTail;
 		setSprite("counter.png");
+		tickEnabled = true;
 	}
 
     @Override
     protected void tick(long deltaTime) {
         if (counterTail.isModifyEnabled() /*c'è un barman libero*/){
+            System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
             counterTail.setModifyEnabled(false);
             Customer customer = counterTail.dequeueCustomer("entry-counter-line-and-movement");
             //gestire i barman;
             //System.out.println("DENTRO CI SONO " + numPeopleInside + " PERSONE");
             customer.moveTo(new Vector(0, 60), "arrived-to-barman");
-            setRotation(90);
             new TimerAction(1200, this, "close-door").execute();
         }
     }
-    
+    /*
     @ActionCallable(name = "barman-is-free")
     public void serveCustomer(Barman barman) {
     	freeBarman = barman;
@@ -36,5 +37,5 @@ public class Counter extends Actor {
     @ActionCallable(name = "get-free-barman")
     public Barman getFreeBarman() {
     	return freeBarman;
-    }
+    }*/
 }
